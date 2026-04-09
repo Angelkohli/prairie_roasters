f849c4ba74caf48e69a081d2230241d6b692fcde
Angel <akohli2@rrc.ca>
2026-04-09 11:01:15 -0500

    Implement province-based GST/PST/HST tax calculation

--- app/models/province.rb
+++ app/models/province.rb
@@ -14,5 +14,6 @@ class Province < ApplicationRecord
 
   def self.ransackable_attributes(auth_object = nil)
     ["name", "abbreviation", "gst", "pst", "hst", "created_at", "updated_at"]
-  end
+  end    
+  
 end
